b0fe9bbda6a9644c609bc334f9bdbb7e0a93bd86
Kimhanjin <ddang0103@naver.com>
2025-05-22 18:37:10 +0900

    Feature: 파일 내용을 읽어 VectorDB에 저장
    파일의 내용을 읽어서, 이를 분할하고(청크단위로), 분할된 텍스트 청크(chunk)들을 OpenAI 임베딩 모델을 사용하여 벡터로 변환한 다음, 최종적으로 Pinecone 벡터 데이터베이스애 저장

--- ingestion.py
+++ ingestion.py
@@ -1,4 +1,28 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 
+load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting:")
+    loader = TextLoader("/c/Users/hammo/Desktop/LLM/intro-to-vector-dbs\mediumblog1.txt")
+    documents = loader.load()
+
+
+    print("Spliting::::")
+    #원본 텍스트 문서를 최대 1000자 길이의 작은 조각들로 나누고, 이 조각들 사이에 겹치는 내용은 없도록
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("Ingesting....")
+    PineconeVectorStore.from_documents(texts,embeddings, index_name=os.environ("INDEX_NAME"))
+
+
+    print("전과정 완료")
